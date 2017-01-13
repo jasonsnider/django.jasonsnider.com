18e3f98d0789e7661008035995cc66e3bfce7ebc
Jason Snider <root@jasonsnider.com>
2017-01-13 07:35:03 -0600

    Added cannonical to the posts table

--- blog/models.py
+++ blog/models.py
@@ -12,8 +12,9 @@ class Post(models.Model):
         (1, 'Draft'),
         (2, 'Published'),
     )
-    title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
+    canonical = models.CharField(max_length=255, unique=True, default='')
+    title = models.CharField(max_length=200)
     body = models.TextField(null=True)
     description = models.CharField(null=True,max_length=500)
     keywords = models.CharField(null=True,max_length=500)
@@ -28,6 +29,7 @@ class Post(models.Model):
         # beforeSave
         super(Post, self).save()
         self.slug = slugify(self.title)
+        self.canonical = '/post/' + self.slug
         super(Post, self).save()
 
     def __str__(self):
